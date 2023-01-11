9449b67828f29fa2f13f575b5162873a65a960b6
Gavin <waltersg298@gmail.com>
2023-01-10 19:10:56 -0700

    starting two number sum problem

---
+++ challenges/two-number-sum.js
@@ -0,0 +1,16 @@
+
+function twoNumberSum(array, targetSum) {
+  if (array.length === 0) {
+    return 'empty array given...';
+  }
+
+}
+
+let input = [3, 5, -4, 8, 11, 1, -1, 6]; 
+// let input = []; 
+
+let target = 10; 
+
+let result = twoNumberSum(input, target); 
+
+console.log(result);
