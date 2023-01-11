fcff2837b1d36fe2d509c304ba4084bf97a38b01
Gavin <waltersg298@gmail.com>
2023-01-10 19:20:09 -0700

    completed

--- challenges/two-number-sum.js
+++ challenges/two-number-sum.js
@@ -4,10 +4,21 @@ function twoNumberSum(array, targetSum) {
     return 'empty array given...';
   }
 
+  let targetSumResult = []; 
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i+1; j < array.length -1; j++) {
+      if (array[i] + array[j] == 10) {
+        targetSumResult.push(array[i]);    
+        targetSumResult.push(array[j]);    
+        return targetSumResult;
+      }
+    }
+  }
+
 }
 
 let input = [3, 5, -4, 8, 11, 1, -1, 6]; 
-// let input = []; 
 
 let target = 10; 
 
